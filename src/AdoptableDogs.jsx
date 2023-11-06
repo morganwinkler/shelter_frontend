bea0330c74fc35fcb19a7984f32b3025dc31010f
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-06 15:33:03 -0500

    style dogs

--- src/AdoptableDogs.jsx
+++ src/AdoptableDogs.jsx
@@ -3,25 +3,26 @@
 export function AdoptableDogs(props) {
   console.log(props);
   return (
-    <div id="dogs">
-      <h1>Adoptable Dogs</h1>
-      {/* loop of defined data from content function aka parent component*/}
-      {props.dogs.map((dog) => (
-        <div key={dog.id}>
-          <h2>{dog.name}</h2>
-          <img src={dog.image_url} alt="dog pics" />
-          <p>Age: {dog.age} </p>
-          <p>Breed: {dog.breed} </p>
-          <div className="dropdown"> 
-            <button className="dropbtn">More Info!</button>
-            <div className="dropdown-content">
-            <a href="#">More Pictures</a>
-            <a href="#">Bio</a>
-            <a href="#">Medical History</a>
+    <div><h1>Adoptable Dogs</h1>
+      <div id="dogs">
+        {/* loop of defined data from content function aka parent component*/}
+        {props.dogs.map((dog) => (
+          <div key={dog.id}>
+            <h2>{dog.name}</h2>
+            <img src={dog.image_url} alt="dog pics" />
+            <p>Age: {dog.age} </p>
+            <p>Breed: {dog.breed} </p>
+            <div className="dropdown"> 
+              <button className="dropbtn">More Info!</button>
+              <div className="dropdown-content">
+              <a href="#">More Pictures</a>
+              <a href="#">Bio</a>
+              <a href="#">Medical History</a>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
