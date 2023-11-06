1e852548f118b5348b71d63535a2423efd4cc558
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-06 14:06:30 -0500

    Moves data to be defined in content and passes it to index
    {props.dogs.map will throw a props validation error - quickfix ignore for this file. Will throw the eslint-disable comment to top of file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import './App.css'
 
 // moves header into its own function
@@ -29,29 +30,22 @@ function ContactForm() {
 }
 
 // moves adoptable dogs into its own function
-function AdoptableDogs() {
+function AdoptableDogs(props) {
+  console.log(props);
   return (
-         <div id="dogs">
+      <div id="dogs">
          <h1>Adoptable Dogs</h1>
-           <div className = 'cards'>
-             <h2>Bogey</h2>
-             <img src="https://media.istockphoto.com/id/1427592301/photo/corgi-puppy-dog-sitting-on-the-table-in-summer-sunny-day-close-up.webp?b=1&s=170667a&w=0&k=20&c=VCgkX-25z4hMZv4ZH1lxbmUvea3YhLE5wRVkb3Q9_6I=" alt="corgi puppy" />
-             <p>Age: 2 <br />Breed: Corgi </p>
-             <button>More Info!</button>
-           </div>
-           <div className = 'cards'>
-             <h2>Dido</h2>
-             <img src="https://media.istockphoto.com/id/1415809177/photo/boxer-puppy-relaxing-on-the-sofa.webp?b=1&s=170667a&w=0&k=20&c=gNth1Bxjwy1U0Szj5yYNuTMgFNju_TasS7o7mRoMR5A=" />
-             <p>Age: 5 <br />Breed: Boxer </p>
-             <button>More Info!</button>
-           </div>
-           <div className = 'cards'>
-             <h2>Stella</h2>
-             <img src="https://media.istockphoto.com/id/1448525820/photo/puppy-of-bernese-mountain-dog-sitting-on-the-grass-in-the-garden-puppy-with-flowers.webp?b=1&s=170667a&w=0&k=20&c=4GHF0EAHkWaBmOlMiFLP9qcOCZIcvT4m3DtVMg95vYw=" />
-             <p>Age: 3 mo <br />Breed: Bernadoodle </p>
-             <button>More Info!</button>
-           </div>
-       </div>
+          {/* loop of defined data from content function aka parent component*/}
+          {props.dogs.map((dog) => (
+            <div key={dog.id}>
+              <h2>{dog.name}</h2>
+              <img src={dog.image_url} alt="dog pics" />
+              <p>Age: {dog.age} </p>
+              <p>Breed: {dog.breed} </p>
+              <button>More Info!</button>
+            </div>
+          ))}
+      </div>
   );
 }
 
@@ -66,10 +60,33 @@ function Footer() {
 
 // creates content function
 function Content() {
+  let dogs = [
+    {
+      id: 1,
+      name: "Bogey",
+      image_url: "https://media.istockphoto.com/id/1427592301/photo/corgi-puppy-dog-sitting-on-the-table-in-summer-sunny-day-close-up.webp?b=1&s=170667a&w=0&k=20&c=VCgkX-25z4hMZv4ZH1lxbmUvea3YhLE5wRVkb3Q9_6I=",
+      age: "2 yr",
+      breed: "corgi",
+    },
+    {
+      id: 2,
+      name: "Dido",
+      image_url: "https://media.istockphoto.com/id/1415809177/photo/boxer-puppy-relaxing-on-the-sofa.webp?b=1&s=170667a&w=0&k=20&c=gNth1Bxjwy1U0Szj5yYNuTMgFNju_TasS7o7mRoMR5A=",
+      age: "5 yr",
+      breed: "boxer",
+    },
+    {
+      id: 3,
+      name: "Stella",
+      image_url: "https://media.istockphoto.com/id/1448525820/photo/puppy-of-bernese-mountain-dog-sitting-on-the-grass-in-the-garden-puppy-with-flowers.webp?b=1&s=170667a&w=0&k=20&c=4GHF0EAHkWaBmOlMiFLP9qcOCZIcvT4m3DtVMg95vYw=",
+      age: "6 mo",
+      breed: "mini bernadoodle",
+    },
+  ];
   return (
     <div>
       <ContactForm />
-      <AdoptableDogs />
+      <AdoptableDogs dogs = {dogs} />
     </div>
   );
 }
