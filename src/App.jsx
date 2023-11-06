f2c25dacd02431da91dec06e4cf9d8d4979b1cda
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-06 13:17:46 -0500

    Add content inside contact form

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,29 @@ import './App.css'
 function App() {
   return (
     <div>
+      {/* header */}
       <header>
         <a href="#">Home</a> | <a href="#dogs">Adoptable Dogs</a> | <a href="#contact">Contact Us</a>
       </header>
-
+      {/* contact form */}
       <div id="contact">
         <h1>Interested in a pup? <br />Contact Us</h1>
+          <div>
+            Your Name <input type="text" />
+          </div>
+          <div>
+            Dog's Name <input type="text" />
+          </div>
+          <div>
+            Phone Number <input type="text" />
+          </div>
+          <button type="submit">Submit</button>
       </div>
-
+      {/* adpoptable dogs */}
       <div id="dogs">
         <h1>Adoptable Dogs</h1>
       </div>
-
+      {/* footer */}
       <footer>
         <p>Copyright 2023</p>
       </footer>
