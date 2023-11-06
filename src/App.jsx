e92d057a3e7b7d72efdb831ce21b1bf9fd9c97fb
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-06 13:14:33 -0500

    Add elements skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import './App.css'
 
 function App() {
-   
+  return (
+    <div>
+      <header>
+        <a href="#">Home</a> | <a href="#dogs">Adoptable Dogs</a> | <a href="#contact">Contact Us</a>
+      </header>
+
+      <div id="contact">
+        <h1>Interested in a pup? <br />Contact Us</h1>
+      </div>
+
+      <div id="dogs">
+        <h1>Adoptable Dogs</h1>
+      </div>
+
+      <footer>
+        <p>Copyright 2023</p>
+      </footer>
+    </div>
+  );  
 }
 
 export default App
